be2043e740445a3cca71e1b6f42f0774bb4623f9
Jeremy Johnson <jeremy@softworks.com.my>
2013-08-14 23:53:55 +0800

    got parallel test from Chrome and Firefox working

--- tddapp/uitests.py
+++ tddapp/uitests.py
@@ -1,20 +1,37 @@
 from pyvows import Vows, expect
 
-from django_pyvows.context import DjangoHTTPContext
+from django_pyvows.context import DjangoContext, DjangoHTTPContext
 from selenium import webdriver
 
 @Vows.batch
-class TDDDjangoApp(DjangoHTTPContext):
+class TDDDjangoApp(DjangoContext):
 
-    def get_settings(self):
-        return "tddapp.settings"
+    
+    class OnFirefox(DjangoHTTPContext):
+        
+        def get_settings(self):
+            return "tddapp.settings"
 
-    def topic(self):
-        self.start_server()
-        browser = webdriver.Firefox()
-        browser.get(self.get_url("/"))
-        return browser
+        def topic(self):
+            self.start_server(port=8888)
+            browser = webdriver.Firefox()
+            browser.get(self.get_url("/"))
+            return browser
 
-    def should_prompt_the_user_with_a_login_page(self, topic):
-        expect(topic.title).to_include('Login')
+        def should_prompt_the_user_with_a_login_page(self, topic):
+            expect(topic.title).to_include('Login')
+    
+    class OnChrome(DjangoHTTPContext):
+        
+        def get_settings(self):
+            return "tddapp.settings"
+
+        def topic(self):
+            self.start_server(port=8887)
+            browser = webdriver.Chrome()
+            browser.get(self.get_url("/"))
+            return browser
+
+        def should_prompt_the_user_with_a_login_page(self, topic):
+            expect(topic.title).to_include('Django')
 
